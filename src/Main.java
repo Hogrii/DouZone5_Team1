e118742b4eb92f217e13bb4b53bfe47f64624798
song <shs1995@naver.com>
2023-04-28 17:41:54 +0900

    fix : main.java

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ public class Main {
 		System.out.println("오늘 회식은 역할맥~~");
 		System.out.println("Git 공부 합시다~!~!!!!!!!!");
 		
-<<<<<<< HEAD
+
+		System.out.println("민아는 배가 고파요");
+		
 		System.out.println("I' heart you");
 		System.out.println("song");
-=======
-		System.out.println("민아는 배가 고파요");
 
->>>>>>> 820074876c43773b64086a54e1d693056e77aa6e
+
 	}
 }
